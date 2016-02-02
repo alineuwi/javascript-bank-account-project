a57991c0e9b64cd38c0432fd54d4f6d870ecee63
kickinbahk <kickinbahk@gmail.com>
2016-02-01 17:56:26 -0800

    Add deposit and withdraw functions

--- bank-account.js
+++ bank-account.js
@@ -1,5 +1,25 @@
+
+var account = {
+    balance: 0
+};
+
 // deposit
+function deposit(accountName, amount) {
+    return accountName.balance += amount;
+}
 
 // withdraw
+function deposit(accountName, amount) {
+    if (amount < accountName.balance){
+        console.log("I am sorry, you do not have enough funds");
+    } else {
+        return accountName.balance += amount;
+    }
+}
 
 // getBalance
+
+
+deposit(account, 25);
+
+
